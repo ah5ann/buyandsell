74bb4196fafa6353a6f7a9a456f48324a460a960
Ahsan Zafar <ahsanzafar96@icloud.com>
2021-05-25 18:23:47 +0100

    search functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Products;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,39 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Search function
+     */
+
+    public function search()
+    {
+        $params=$_GET;
+        if($params['pfrom'])
+        {
+            if($params['pto'])
+            {
+                $products=Products::where('price','>',$params['pfrom'])->where('price','<',$params['pto'])
+                ->orderBy('price', $params['sortp']?($params['sortp']==1?'desc':'asc'):'desc')
+                ->get();
+            }
+            else
+            {
+                $products=Products::where('price','>',$params['pfrom'])
+                ->orderBy('price', $params['sortp']?($params['sortp']==1?'desc':'asc'):'desc')
+                ->get();
+            }
+        }
+        elseif($params['sortp'])
+        {
+            $products=Products::orderby('price', $params['sortp']==1?'desc':'asc')->get();
+        }
+        else
+        {
+            $products=Products::all();
+        }
+        
+        return view('products', ['title' => 'Search Results', 'products'=> $products]);
     }
 }
